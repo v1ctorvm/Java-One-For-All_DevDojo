d3e7b3fbbecf0f7bffaf0d0e1f5df6d885e26c4d
v1ctorvm <vitinmtxs@gmail.com>
2025-12-29 20:34:51 -0300

    aulas

---
+++ src/javaoneforall/introduction/Class01HelloDevDojo.java
@@ -0,0 +1,8 @@
+package javaoneforall.introduction;
+
+public class Class01HelloDevDojo {
+    public static void main(String[] args){
+        System.out.println("Hello World (Again)");
+    }
+
+}
---
+++ src/javaoneforall/introduction/Class02PrimitiveTypes.java
@@ -0,0 +1,24 @@
+package javaoneforall.introduction;
+
+public class Class02PrimitiveTypes {
+    public static void main(String[] args){
+
+
+        // int, double, float, boolean, char, byte, short, long
+        int age = 10;
+        long bugNumber = 10000;
+        float salaryFloat = 2500;
+        double salaryDouble = 5000;
+        boolean trueValue = true;
+        boolean falseValue = false;
+        short ageShort = 28;
+        byte ageByte = 39;
+        char character = 87; // Posso colocar numero da tabela ASCII / Unicode
+
+
+        System.out.println("The age is " +age+ " years ols");
+
+        System.out.println("character = " + character);
+
+    }
+}
