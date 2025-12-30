249fe99e962afb4568650efb43067a7e326d808d
v1ctorvm <vitinmtxs@gmail.com>
2025-12-30 19:30:39 -0300

    Estudo

---
+++ src/javaoneforall/introduction/Class03PrimitiveTypesExercice.java
@@ -0,0 +1,14 @@
+package javaoneforall.introduction;
+
+public class Class03PrimitiveTypesExercice {
+    static void main() {
+
+        String name = "Kirito";
+        String adress = "439 South District";
+        double salary = 5340.21;
+        // DD/MM/YYYY
+        String recievedSalaryDate = "21/12/2021";
+        String report = "I " + name + " living at " + adress + ", confirm that I received the salary of " + salary + " on date " +recievedSalaryDate;
+        System.out.println(report);
+    }
+}
