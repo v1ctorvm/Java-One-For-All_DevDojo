9dd086f424823cf746014ca0042e4295565112e6
v1ctorvm <vitinmtxs@gmail.com>
2025-12-30 19:30:35 -0300

    Estudo

--- src/javaoneforall/introduction/Class02PrimitiveTypes.java
+++ src/javaoneforall/introduction/Class02PrimitiveTypes.java
@@ -5,7 +5,7 @@ public class Class02PrimitiveTypes {
 
 
         // int, double, float, boolean, char, byte, short, long
-        int age = 10;
+        int age = 121575219;
         long bugNumber = 10000;
         float salaryFloat = 2500;
         double salaryDouble = 5000;
@@ -16,9 +16,27 @@ public class Class02PrimitiveTypes {
         char character = 87; // Posso colocar numero da tabela ASCII / Unicode
 
 
+        // cast - Precisa ter certeza que as coisas irà funcionar.
+        int age2 = (int)100000000000L;
+        System.out.println("age = " + age2);
+
         System.out.println("The age is " +age+ " years ols");
 
         System.out.println("character = " + character);
+
+
+
+        String name = "Goku";
+
+        String hiName = "Hi, im " + name; // COMO ESSA PORRA FUNCIONA??
+        System.out.println("Name: " + name);
+
+        System.out.println(hiName);
+
+
+
+
+
 
     }
 }
