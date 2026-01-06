17d7384bdb503ab62130e5a0787c36885b13f16b
v1ctorvm <vitinmtxs@gmail.com>
2026-01-06 08:11:56 -0300

    FEAT: Array Multimentional

---
+++ src/javaoneforall/introduction/Class08MultidimensionalArrays.java
@@ -0,0 +1,25 @@
+package javaoneforall.introduction;
+
+public class Class08MultidimensionalArrays {
+    static void main() {
+
+        int[][] days = new int[3][3];
+        days[0][0] = 31;
+        days[0][1] = 28;
+        days[0][2] = 31;
+        days[1][0] = 30;
+        days[1][1] = 31;
+        days[1][2] = 30;
+        days[2][0] = 30;
+        days[2][1] = 31;
+        days[2][2] = 30;
+
+        for (int i = 0; i < days.length; i++) {
+            for (int j = 0; j < days[i].length; j++) {
+                System.out.println(days[i][j]);
+            }
+        }
+
+
+    }
+}
