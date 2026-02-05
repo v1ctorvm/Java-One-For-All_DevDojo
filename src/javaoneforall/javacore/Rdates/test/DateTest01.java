91a7f53743358cf7e869ff54bcceb38886141b04
v1ctorvm <vitinmtxs@gmail.com>
2026-02-05 20:21:55 -0300

    FEAT: Utility Classes - Date

---
+++ src/javaoneforall/javacore/Rdates/test/DateTest01.java
@@ -0,0 +1,11 @@
+package javaoneforall.javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    static void main() {
+
+        Date date = new Date(1770333355155L); // Works from Milisseconds
+        System.out.println(date);
+    }
+}
