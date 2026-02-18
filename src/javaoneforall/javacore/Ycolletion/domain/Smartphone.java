40cc0c303c41d06f7ed6c5f6b1036788a5c6db03
v1ctorvm <vitinmtxs@gmail.com>
2026-02-18 13:08:57 -0300

    FEAT: Collection - hashCode

--- src/javaoneforall/javacore/Ycolletion/domain/Smartphone.java
+++ src/javaoneforall/javacore/Ycolletion/domain/Smartphone.java
@@ -19,6 +19,15 @@ public class Smartphone {
         return serialNumber != null && serialNumber.equals(obj1.serialNumber);
     }
 
+    // if x.equals(y) == true, y.hashCode() == x.hashCode();
+    // y.hashCode() == x.hashCode() == doesnt mean that y.equals(x) == true;
+    // x.equals(y) == false
+    // y.hashCode() != x.hashCode() x.equals(y) should be false
+    @Override
+    public int hashCode() {
+        return  serialNumber == null ? 0 : serialNumber.hashCode();
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
